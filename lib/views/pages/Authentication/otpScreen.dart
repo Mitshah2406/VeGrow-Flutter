b9a8a8572c22f2dfdd810797044f90608e0ccecd
mitshah2406 <mitshah2406@gmail.com>
2023-03-18 14:12:11 +0530

    login ui

--- lib/views/pages/Authentication/otpScreen.dart
+++ lib/views/pages/Authentication/otpScreen.dart
@@ -4,9 +4,17 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:vegrow/consts/appConstant.dart';
 
-class OtpScreen extends StatelessWidget {
-  const OtpScreen({Key? key}) : super(key: key);
+class OtpScreen extends StatefulWidget {
+   OtpScreen({Key? key}) : super(key: key);
 
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  FocusNode noteFocus = FocusNode();
+
+  // late BuildContext context;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +22,7 @@ class OtpScreen extends StatelessWidget {
       backgroundColor: AppConstant.bgColorAuth,
       body: SafeArea(
           child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 32.0),
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
         child: Column(children: [
           Align(
               alignment: Alignment.topLeft,
@@ -52,7 +60,7 @@ class OtpScreen extends StatelessWidget {
             height: 10,
           ),
           Text(
-            "Enter Your Phone Number and We'll Send You a Verification Code",
+            "Enter OTP",
             style: TextStyle(
                 fontSize: 14,
                 fontWeight: FontWeight.bold,
@@ -68,71 +76,78 @@ class OtpScreen extends StatelessWidget {
                 color: Colors.white, borderRadius: BorderRadius.circular(12)),
             child: Column(
               children: [
-                _textFieldOTP( true, false),
-                // TextFormField(
-                //   keyboardType: TextInputType.number,
-                //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                //   decoration: InputDecoration(
-                //       enabledBorder: OutlineInputBorder(
-                //           borderSide: BorderSide(color: Colors.black12),
-                //           borderRadius: BorderRadius.circular(18)),
-                //       focusedBorder: OutlineInputBorder(
-                //           borderSide: BorderSide(color: Colors.black12),
-                //           borderRadius: BorderRadius.circular(18)),
-                //       prefix: Padding(
-                //         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                //         child: Text(
-                //           "+91",
-                //           style: TextStyle(
-                //               fontSize: 18, fontWeight: FontWeight.bold),
-                //         ),
-                //       ),
-                //       suffixIcon: Icon(
-                //         Icons.check_circle,
-                //         color: Colors.green,
-                //         size: 32,
-                //       )),
-                // ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _textFieldOTP(first: true, last: false),
+                    _textFieldOTP(first: false, last: false),
+                    _textFieldOTP(first: false, last: false),
+                    _textFieldOTP(first: false, last: true),
+                  ],
+                ),
                 SizedBox(
                   height: 22,
-                )
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        // Get.toNamed('/sendOtp');
+                      },
+                      style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.purple),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(24)))),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          "Verify",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Didnt Revieve The Code? ", style: TextStyle(fontWeight:FontWeight.bold, fontSize: 16, color: Colors.black26)),
+                SizedBox(height: 15,),
+                Text("Resend Code", style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.purple))
               ],
             ),
           ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/sendOtp');
-                },
-                style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.purple),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24)))),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    "Send OTP",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                )),
-          )
         ]),
       )),
     );
   }
-    _textFieldOTP([ bool ?first, last]){
-      return Container(
-        height: 85,
-        child: AspectRatio(aspectRatio: 
-        1.9,
+
+  Widget _textFieldOTP({bool ?first, last}) {
+    // final BuildContext context;
+    return Container(
+      height: 85,
+      child: AspectRatio(
+        aspectRatio: .7,
         child: TextField(
+          // focusNode: noteFocus,
           autofocus: true,
-          onChanged: (value){},
+          onChanged: (value) {
+            if (value.length == 1 && last == false) {
+              FocusScope.of(context).nextFocus();
+              // noteFocus.nextFocus();
+            }
+            if (value.length == 0 && first == false) {
+              FocusScope.of(context).previousFocus();
+              // noteFocus.previousFocus();
+            }
+          },
           showCursor: false,
           readOnly: false,
           textAlign: TextAlign.center,
@@ -140,11 +155,202 @@ class OtpScreen extends StatelessWidget {
           keyboardType: TextInputType.number,
           maxLength: 1,
           decoration: InputDecoration(
+            // contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
             counter: Offstage(),
-            // enabledBorder: OutlineInputBorder(borderRadius: )
-
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(width: 1, color: Colors.black12),
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(width: 1, color: Colors.purple)),
           ),
-        ),),
-      );
-    }
+        ),
+      ),
+    );
+  }
 }
+// import 'package:flutter/material.dart';
+
+// class OtpScreen extends StatefulWidget {
+//   const OtpScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _OtpScreenState createState() => _OtpScreenState();
+// }
+
+// class _OtpScreenState extends State<OtpScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       backgroundColor: Color(0xfff7f6fb),
+//       body: SafeArea(
+//         child: Padding(
+//           padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+//           child: Column(
+//             children: [
+//               Align(
+//                 alignment: Alignment.topLeft,
+//                 child: GestureDetector(
+//                   onTap: () => Navigator.pop(context),
+//                   child: Icon(
+//                     Icons.arrow_back,
+//                     size: 32,
+//                     color: Colors.black54,
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 18,
+//               ),
+//               Container(
+//                 width: 200,
+//                 height: 200,
+//                 decoration: BoxDecoration(
+//                   color: Colors.deepPurple.shade50,
+//                   shape: BoxShape.circle,
+//                 ),
+//                 child: Image.asset(
+//                   'assets/images/illustration-3.png',
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 24,
+//               ),
+//               Text(
+//                 'Verification',
+//                 style: TextStyle(
+//                   fontSize: 22,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 10,
+//               ),
+//               Text(
+//                 "Enter your OTP code number",
+//                 style: TextStyle(
+//                   fontSize: 14,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.black38,
+//                 ),
+//                 textAlign: TextAlign.center,
+//               ),
+//               SizedBox(
+//                 height: 28,
+//               ),
+//               Container(
+//                 padding: EdgeInsets.all(28),
+//                 decoration: BoxDecoration(
+//                   color: Colors.white,
+//                   borderRadius: BorderRadius.circular(12),
+//                 ),
+//                 child: Column(
+//                   children: [
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         _textFieldOTP(first: true, last: false),
+//                         _textFieldOTP(first: false, last: false),
+//                         _textFieldOTP(first: false, last: false),
+//                         _textFieldOTP(first: false, last: true),
+//                       ],
+//                     ),
+//                     SizedBox(
+//                       height: 22,
+//                     ),
+//                     SizedBox(
+//                       width: double.infinity,
+//                       child: ElevatedButton(
+//                         onPressed: () {},
+//                         style: ButtonStyle(
+//                           foregroundColor:
+//                               MaterialStateProperty.all<Color>(Colors.white),
+//                           backgroundColor:
+//                               MaterialStateProperty.all<Color>(Colors.purple),
+//                           shape:
+//                               MaterialStateProperty.all<RoundedRectangleBorder>(
+//                             RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(24.0),
+//                             ),
+//                           ),
+//                         ),
+//                         child: Padding(
+//                           padding: EdgeInsets.all(14.0),
+//                           child: Text(
+//                             'Verify',
+//                             style: TextStyle(fontSize: 16),
+//                           ),
+//                         ),
+//                       ),
+//                     )
+//                   ],
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 18,
+//               ),
+//               Text(
+//                 "Didn't you receive any code?",
+//                 style: TextStyle(
+//                   fontSize: 14,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.black38,
+//                 ),
+//                 textAlign: TextAlign.center,
+//               ),
+//               SizedBox(
+//                 height: 18,
+//               ),
+//               Text(
+//                 "Resend New Code",
+//                 style: TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.purple,
+//                 ),
+//                 textAlign: TextAlign.center,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _textFieldOTP({bool? first, last}) {
+//     return Container(
+//       height: 85,
+//       child: AspectRatio(
+//         aspectRatio: 1.0,
+//         child: TextField(
+//           autofocus: true,
+//           onChanged: (value) {
+//             if (value.length == 1 && last == false) {
+//               FocusScope.of(context).nextFocus();
+//             }
+//             if (value.length == 0 && first == false) {
+//               FocusScope.of(context).previousFocus();
+//             }
+//           },
+//           showCursor: false,
+//           readOnly: false,
+//           textAlign: TextAlign.center,
+//           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//           keyboardType: TextInputType.number,
+//           maxLength: 1,
+//           decoration: InputDecoration(
+//             counter: Offstage(),
+//             enabledBorder: OutlineInputBorder(
+//                 borderSide: BorderSide(width: 2, color: Colors.black12),
+//                 borderRadius: BorderRadius.circular(12)),
+//             focusedBorder: OutlineInputBorder(
+//                 borderSide: BorderSide(width: 2, color: Colors.purple),
+//                 borderRadius: BorderRadius.circular(12)),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
